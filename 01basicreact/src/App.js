9266cc1f44e9edeaf5afa04de9071134d16038a4
ManpreetKaur0803 <mini08032003@gmail.com>
2025-08-25 21:28:23 +0530

    Basic setup

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>Chai aur React|Manpreet Kaur</h1>;
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,5 @@
+function App() {
+  return <h1>Chai aur react with Vite| Manpreet Kaur</h1>;
+}
+
+export default App;
