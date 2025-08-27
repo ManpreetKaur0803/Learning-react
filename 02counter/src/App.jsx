bed304b43239041b7cc2b5c468dfe31cb6dc5529
ManpreetKaur0803 <mini08032003@gmail.com>
2025-08-28 01:40:48 +0530

    Hooks example Counter miniproject

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,40 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  //***********This is an Hooks example project */
+  let [counter, setCounter] = useState(5); //5 is default value of counter
+
+  let addValue = () => {
+    // setCounter(counter + 1);
+
+    if (counter < 20) {
+      // counter = counter + 1;
+      setCounter(counter + 1);
+    }
+  };
+  let reduceValue = () => {
+    // setCounter(counter - 1);
+
+    if (counter > 0) {
+      // counter = counter - 1;
+      setCounter(counter - 1);
+    }
+  };
+  return (
+    <>
+      <h1>Chai and Counter Project</h1>
+      <h2>Counter Value: {counter}</h2>
+      <button onClick={addValue}>Count Up</button>
+      <button onClick={reduceValue}>Count down</button>
+      <br />
+      <br />
+      <br />
+      <footer>So count is {counter}</footer>
+    </>
+  );
+}
+
+export default App;
