edefc8e7679424341f421ad76268ef806e6dd7ba
ManpreetKaur0803 <mini08032003@gmail.com>
2025-08-28 00:48:27 +0530

    custom react and js in jsx

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,11 +1,15 @@
 import Chai from "./chai";
 function App() {
+  const username = "chai aur code"; 
   return (
     <>
       <Chai />
-      <h3>Chai with snacks</h3>
+      <h3>Chai with snacks | username: {username}</h3>    
     </>
   );
 }
 
 export default App;
+
+
+//*******{username} THIS IS HOW JAVASCRIPT IN JSX IS DEFINED AND HERE WE CAN ONLY WRITE EVALUATED EXPRESSION.*******
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,9 +1,38 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+// const reactElement = {
+//   type: "a",
+//   props: {
+//     href: "https://google.com",
+//     target: "_blank",
+//   },
+//   children: "Click me to visit Google",
+// };
+// ***********AS WE DON'T KNOW WHAT TYPE OF ARGUMENT AND PARAMETER REACT IS EXPECTING IN THE INPUT*************//
+
+const anotherUser = "Chai aur React";
+const ReactElement = React.createElement(
+  "a",
+  { href: "https://google.com", target: "_blank" },
+  "Click me to visit google ",
+  anotherUser
+);
+
+const otherElement = (
+  <a href="https://google.com" target="_blank">
+    <br />
+    Visit Google
+  </a>
+);
+
+createRoot(document.getElementById("root")).render(
+  <>
+    {ReactElement}
+    {otherElement}
     <App />
-  </StrictMode>,
-)
+  </>
+);
+
+// createRoot(document.getElementById("root")).render(<App />);
